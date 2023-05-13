d77a71597f2bf789803add8e3bc54c1b5ba69b4a
Alan Mutalibov <alanasia904@gmail.com>
2023-05-13 15:02:33 +0300

    Added ability to insert an item of Department, but without validation

--- CoursLoadAccounting/AddMember.xaml.cs
+++ CoursLoadAccounting/AddMember.xaml.cs
@@ -31,8 +31,6 @@ namespace CoursLoadAccounting
 
         private List<Dictionary<int, int>> getIdFromComboboxes = new List<Dictionary<int, int>>();
 
-        //private Dictionary<int, int> getIdFromComboboxes = new Dictionary<int, int>();
-        
         public AddMember(int ind, PostgresDataBase databaseUniversity)
         {
             this.databaseUniversity = databaseUniversity;
@@ -40,7 +38,34 @@ namespace CoursLoadAccounting
             InitializeComponent();
 
             if (ind == 0) { }
-            if (ind == 1) { }
+            if (ind == 1) 
+            {
+                this.Title = "Добавление кафедры";
+                this.Height = 355;
+
+                labels.Add(GetLabel("Название кафедры:"));
+                textBoxes.Add(GetTextBox());
+
+                labels.Add(GetLabel("Завкафедрой:"));
+                comboBoxes.Add(GetMembers());
+                comboBoxes[0].SelectionChanged += OtherMembers_Click;                
+
+                labels.Add(GetLabel("Факультет:"));
+                comboBoxes.Add(GetFacultets());
+                comboBoxes[1].SelectionChanged += OtherFaculty_Click;               
+
+                Button AddKafedra = GetButton();
+               
+                AddKafedra.Click += AddKafedra_Click;
+
+                addPanel.Children.Add(labels[0]);
+                addPanel.Children.Add(textBoxes[0]);
+                addPanel.Children.Add(labels[1]);
+                addPanel.Children.Add(comboBoxes[0]);
+                addPanel.Children.Add(labels[2]);
+                addPanel.Children.Add(comboBoxes[1]);
+                addPanel.Children.Add(AddKafedra);
+            }
             if (ind == 2) 
             {
                 this.Height = 290;
@@ -51,30 +76,9 @@ namespace CoursLoadAccounting
 
                 labels.Add(GetLabel("Выберетие декана:"));
 
-                comboBoxes.Add(GetComboBox());
+                comboBoxes.Add(GetMembers());
+                comboBoxes[0].SelectionChanged += OtherMembers_Click;
                 
-                databaseUniversity.Open();
-                var readerMembers = databaseUniversity.ExecuteQuery("SELECT id, CONCAT(firstname, ' ', middlename, ', ', phonenumber) FROM departmentmembers");
-
-                if (readerMembers.HasRows)
-                {
-                    int i = 0;
-                    Dictionary<int, int> getId = new Dictionary<int, int>();
-                    while (readerMembers.Read())
-                    {
-                        getId.Add(i, (int)readerMembers.GetValue(0));
-                        comboBoxes[0].Items.Add((string)readerMembers.GetValue(1));
-                        i++;
-                    }
-                    getIdFromComboboxes.Add(getId);
-                }
-
-                databaseUniversity.Close();
-
-                comboBoxes[0].Items.Add("Другой");
-                comboBoxes[0].SelectionChanged += OtherFaculty_Click;
-                comboBoxes[0].SelectedIndex = 0;
-
                 Button AddFaculty = GetButton();
                 AddFaculty.Click += AddFaculty_Click;
 
@@ -126,18 +130,71 @@ namespace CoursLoadAccounting
             if (ind == 4) { }
             if (ind == 5) { }           
         }
-
-        private void AddMembers_Click(object sender, RoutedEventArgs e) // Сделать проверку на допустимость значений.
+        private ComboBox GetMembers()
         {
+            ComboBox comboBox = GetComboBox();
+
             databaseUniversity.Open();
-            databaseUniversity.ExecuteNonQuery($"CALL add_departmentmember('{textBoxes[0].Text}', '{textBoxes[1].Text}', '{textBoxes[4].Text}', " +
-                                                                         $"'{textBoxes[2].Text}', '{textBoxes[3].Text}');");
+
+            var readerMembers = databaseUniversity.ExecuteQuery("SELECT id, CONCAT(firstname, ' ', middlename, ', ', phonenumber) FROM departmentmembers");
+
+            if (readerMembers.HasRows)
+            {
+                int i = 0;
+
+                Dictionary<int, int> getId = new Dictionary<int, int>();
+
+                while (readerMembers.Read())
+                {
+                    getId.Add(i, (int)readerMembers.GetValue(0));
+                    comboBox.Items.Add((string)readerMembers.GetValue(1));
+                    i++;
+                }
+
+                getIdFromComboboxes.Add(getId);
+            }
+
             databaseUniversity.Close();
-            
-            this.Close();
+
+            comboBox.Items.Add("Другой");
+            comboBox.SelectedIndex = 0;
+
+            return comboBox;
         }
 
-        private void OtherFaculty_Click(object sender, SelectionChangedEventArgs e)
+        private ComboBox GetFacultets()
+        {
+            ComboBox comboBox = GetComboBox();
+
+            databaseUniversity.Open();
+
+            var reader = databaseUniversity.ExecuteQuery("SELECT id, name FROM faculty");
+
+            if (reader.HasRows)
+            {
+                int i = 0;
+
+                Dictionary<int, int> getId = new Dictionary<int, int>();
+
+                while (reader.Read())
+                {
+                    getId.Add(i, (int)reader.GetValue(0));
+                    comboBox.Items.Add((string)reader.GetValue(1));
+                    i++;
+                }
+
+                getIdFromComboboxes.Add(getId);
+            }
+
+            databaseUniversity.Close();
+
+            comboBox.Items.Add("Другой");           
+            comboBox.SelectedIndex = 0;
+
+            return comboBox;
+        }
+        
+        private void OtherMembers_Click(object sender, SelectionChangedEventArgs e)
         {
             int n = comboBoxes[0].Items.Count - 1;
             if (comboBoxes[0].SelectedIndex == n)
@@ -145,40 +202,70 @@ namespace CoursLoadAccounting
                 AddMember addMember = new AddMember(3, databaseUniversity);
                 addMember.ShowDialog();
 
-                MessageBox.Show("Добавлено");
-
                 databaseUniversity.Open();
 
                 int count = (int)(long)databaseUniversity.ExecuteScalar("SELECT COUNT(*) FROM departmentmembers");
-                if(count > n)
+                if (count > n)
                 {
-                    comboBoxes[0].Items.RemoveAt(n);
-
+                    
                     var readerMembers = databaseUniversity.ExecuteQuery("SELECT id, CONCAT(firstname, ' ', middlename, ', ', phonenumber) as dek FROM departmentmembers " +
                                                                         "ORDER BY id DESC LIMIT 1");
                     if (readerMembers.HasRows)
                     {
+                        comboBoxes[0].Items.RemoveAt(n);
+
                         readerMembers.Read();
-                        
+
                         comboBoxes[0].Items.Add((string)readerMembers["dek"]);
 
-                        getIdFromComboboxes[0].Add(n, (int)readerMembers["id"]);                        
-                    }
+                        getIdFromComboboxes[0].Add(n, (int)readerMembers["id"]);
 
-                    comboBoxes[0].Items.Add("Другой");
-                    comboBoxes[0].SelectedIndex = comboBoxes[0].Items.Count - 2;
-                    
-                    addPanel.Children.RemoveAt(3);
-                    addPanel.Children.RemoveAt(3);
-                    addPanel.Children.Add(comboBoxes[0]);
-
-                    Button AddFaculty = GetButton();
-                    AddFaculty.Click += AddFaculty_Click;
-                    addPanel.Children.Add(AddFaculty);
+                        comboBoxes[0].Items.Add("Другой");
+                        comboBoxes[0].SelectedIndex = comboBoxes[0].Items.Count - 2;
+                    }                    
                 }
+                else
+                    comboBoxes[0].SelectedIndex = 0;
+
+                databaseUniversity.Close();
+            }          
+        }
+
+        private void OtherFaculty_Click(object sender, SelectionChangedEventArgs e)
+        {
+            int n = comboBoxes[1].Items.Count - 1;
+            if (comboBoxes[1].SelectedIndex == n)
+            {
+                AddMember addMember = new AddMember(2, databaseUniversity);
+                addMember.ShowDialog();
+
+                databaseUniversity.Open();
+
+                int count = (int)(long)databaseUniversity.ExecuteScalar("SELECT COUNT(*) FROM faculty");
+                if (count > n)
+                {                    
+                    var reader = databaseUniversity.ExecuteQuery("SELECT id, name FROM faculty " +
+                                                                        "ORDER BY id DESC LIMIT 1");
+                    if (reader.HasRows)
+                    {
+                        comboBoxes[1].Items.RemoveAt(n);
+
+                        reader.Read();
+
+                        comboBoxes[1].Items.Add((string)reader["name"]);
+
+                        getIdFromComboboxes[1].Add(n, (int)reader["id"]);
+
+                        comboBoxes[1].Items.Add("Другой");
+                        comboBoxes[1].SelectedIndex = comboBoxes[1].Items.Count - 2;
+                    }                                        
+                }
+                else
+                    comboBoxes[1].SelectedIndex = 0;
 
                 databaseUniversity.Close();
             }
+            
         }
 
         private void AddFaculty_Click(object sender, RoutedEventArgs e) // Сделать проверку на допустимость символов
@@ -188,6 +275,26 @@ namespace CoursLoadAccounting
             
             databaseUniversity.ExecuteNonQuery($"CALL add_faculty('{textBoxes[0].Text}', {getIdFromComboboxes[0][comboBoxes[0].SelectedIndex]});");
 
+            databaseUniversity.Close();
+
+            this.Close();
+        }
+
+        private void AddMembers_Click(object sender, RoutedEventArgs e) // Сделать проверку на допустимость значений.
+        {
+            databaseUniversity.Open();
+            databaseUniversity.ExecuteNonQuery($"CALL add_departmentmember('{textBoxes[0].Text}', '{textBoxes[1].Text}', '{textBoxes[4].Text}', " +
+                                                                         $"'{textBoxes[2].Text}', '{textBoxes[3].Text}');");
+            databaseUniversity.Close();
+
+            this.Close();
+        }
+
+        private void AddKafedra_Click(object sender, RoutedEventArgs e)
+        {         
+            databaseUniversity.Open();
+            databaseUniversity.ExecuteNonQuery($"CALL add_kafedra('{textBoxes[0].Text}', " +
+                                               $"{getIdFromComboboxes[0][comboBoxes[0].SelectedIndex]}, {getIdFromComboboxes[1][comboBoxes[1].SelectedIndex]});");
             databaseUniversity.Close();
 
             this.Close();
