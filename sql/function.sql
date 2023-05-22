65707620027f0ece90cb13c7c19902c8b1e07265
Alan Mutalibov <alanasia904@gmail.com>
2023-05-22 19:47:59 +0300

    Corrected update method in sql and AddWindow

--- sql/function.sql
+++ sql/function.sql
@@ -85,7 +85,7 @@ BEGIN
                       substr(phone_number, 10, 2);
     ELSE
       -- Если номер телефона неправильной длины, выбрасываем исключение
-      RAISE EXCEPTION 'Invalid phone number';
+      RAISE EXCEPTION 'Телефон введен неверно!';
     END IF;
 
     RETURN phone_number;
@@ -112,7 +112,7 @@ BEGIN
       RETURN femail;
     ELSE
       -- Если адрес электронной почты неправильного формата, выбрасываем исключение
-      RAISE EXCEPTION 'Invalid email address';
+      RAISE EXCEPTION 'Почта введенна неверно!';
     END IF;
   END;
 END;
--- sql/procedure.sql
+++ sql/procedure.sql
@@ -28,8 +28,13 @@ CREATE OR REPLACE PROCEDURE update_departmentmember(
     IN p_middlename TEXT DEFAULT NULL,
     IN p_email TEXT DEFAULT NULL
 )
-LANGUAGE SQL
 AS $$
+BEGIN
+    p_phonenumber := format_phone_number(p_phonenumber);
+    IF CheckMembersPhone(p_phonenumber) THEN
+        RAISE EXCEPTION 'Данный номер уже занят!';
+    END IF;
+    p_email := validate_email(p_email);
     UPDATE departmentmembers
     SET firstname = p_firstname,
         secondname = p_secondname,
@@ -37,48 +42,55 @@ AS $$
         email = p_email,
         phonenumber = p_phonenumber
     WHERE id = p_id;
-$$;
+END
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE delete_departmentmember(
     IN p_id INT
 )
-LANGUAGE SQL
 AS $$
+BEGIN
     DELETE FROM departmentmembers
     WHERE id = p_id;
-$$;
+END
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE add_faculty(
     IN p_name TEXT,
     IN p_department_id INTEGER
 )
-LANGUAGE SQL
+
 AS $$
+BEGIN
     INSERT INTO Faculty (name, departamentID)
     VALUES (p_name, p_department_id);
-$$;
+END
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE update_faculty(
     IN p_id INT,
     IN p_name TEXT,
     IN p_department_id INTEGER
 )
-LANGUAGE SQL
+
 AS $$
+BEGIN
     UPDATE Faculty
     SET name = p_name,
         departamentID = p_department_id
     WHERE id = p_id;
-$$;
+END
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE delete_faculty(
     IN p_id INT
 )
-LANGUAGE SQL
 AS $$
+BEGIN
     DELETE FROM Faculty
     WHERE id = p_id;
-$$;
+END
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE add_kafedra(
     IN p_name TEXT,
@@ -99,23 +111,26 @@ CREATE OR REPLACE PROCEDURE update_kafedra(
     IN p_department_id INTEGER,
     IN p_faculty_id INTEGER
 )
-LANGUAGE SQL
 AS $$
+BEGIN
+    p_name := CheckKafedra(p_name);
     UPDATE Kafedra
     SET name = p_name,
         departamentID = p_department_id,
         facultyID = p_faculty_id
     WHERE id = p_id;
-$$;
+END
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE delete_kafedra(
     IN p_id INT
 )
-LANGUAGE SQL
 AS $$
+BEGIN
     DELETE FROM Kafedra
     WHERE id = p_id;
-$$;
+END
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE add_speciality(
     IN p_name TEXT,
@@ -137,23 +152,27 @@ CREATE OR REPLACE PROCEDURE update_speciality(
     IN p_code TEXT,
     IN p_faculty_id INTEGER
 )
-LANGUAGE SQL
 AS $$
+BEGIN
+    p_name := CheckNameSpec(p_name);
+    p_code := CheckCodeSpec(p_code);
     UPDATE Speciality
     SET name = p_name,
         code = p_code,
         facultyID = p_faculty_id
     WHERE id = p_id;
-$$;
+END
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE delete_speciality(
     IN p_id INT
 )
-LANGUAGE SQL
 AS $$
+BEGIN
     DELETE FROM Speciality
     WHERE id = p_id;
-$$;
+END
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE add_discipline(
     IN p_name TEXT,
@@ -172,22 +191,25 @@ CREATE OR REPLACE PROCEDURE update_discipline(
     IN p_name TEXT,
     IN p_kafedra_id INTEGER
 )
-LANGUAGE SQL
 AS $$
+BEGIN
+    p_name := CheckDiscipline(p_name);
     UPDATE Discipline
     SET name = p_name,
         kafedraID = p_kafedra_id
     WHERE id = p_id;
-$$;
+END
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE delete_discipline(
     IN p_id INT
 )
-LANGUAGE SQL
 AS $$
+BEGIN
     DELETE FROM Discipline
     WHERE id = p_id;
-$$;
+END
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE add_discipline_speciality(
     IN p_discipline_id INTEGER,
@@ -216,8 +238,9 @@ CREATE OR REPLACE PROCEDURE update_discipline_speciality(
     IN p_prhours INTEGER DEFAULT NULL,
     IN p_labs INTEGER DEFAULT NULL
 )
-LANGUAGE SQL
 AS $$
+BEGIN
+    PERFORM CheckUchet(p_discipline_id, p_speciality_id);
     UPDATE DisciplineSpeciality
     SET DisciplineID = p_discipline_id,
         SpecialityID = p_speciality_id,
@@ -227,14 +250,17 @@ AS $$
         semestr = p_semestr,
         sessia = p_sessia
     WHERE id = p_id;
-$$;
+END
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE delete_discipline_speciality(
     IN p_id INT
 )
-LANGUAGE SQL
+
 AS $$
+BEGIN
     DELETE FROM DisciplineSpeciality
     WHERE id = p_id;
-$$;
+END
+$$ LANGUAGE plpgsql;
 
